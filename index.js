07e7f2f7cce8c799fcbde2220a98b1c851fc9371
Babsmutini <scotteyawo@gmail.com>
2021-08-17 19:49:07 +0100

    First Commit
    Program to print out your name

---
+++ index.js
@@ -0,0 +1,7 @@
+let  userName = prompt("Please enter your name", "Enter Your Name");
+
+if (userName!= null) {
+    alert("Your Name is " +userName+ " ");
+    console.log("Your Name is "+userName+" ");
+}
+
